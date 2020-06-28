0cdf2eb5fe39d628b0bc210db78ef7996ee616f8
nvdinh09thxd <nvdinh09thxd@gmail.com>
2020-06-28 14:52:12 +0700

    edit baihoclop

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,13 +2,14 @@ package controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Account;
+import model.bean.Account;
 
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -19,20 +20,24 @@ public class LoginController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect(request.getContextPath() + "/baitap/BT1/login.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("demo/login.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		if ("".equals(username) || "".equals(password)) {
-			response.sendRedirect(request.getContextPath() + "/baitap/BT1/login.jsp?err=0");
-		} else {
-			Account user = new Account(username, password);
-			HttpSession session = request.getSession();
-			session.setAttribute("user", user);
-			response.sendRedirect(request.getContextPath() + "/baitap/BT1/welcome.jsp");
-		}
+		Account account = new Account(username, password);
+
+		HttpSession session = request.getSession();
+		session.setAttribute("account", account);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("demo/welcome.jsp");
+		rd.forward(request, response);
 	}
+
 }
--- src/controller/MuaHoaController.java
+++ src/controller/MuaHoaController.java
@@ -4,63 +4,66 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Hoa;
+import model.bean.Hoa;
 
 public class MuaHoaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	List<Hoa> listHoa = new ArrayList<Hoa>();
 
 	public MuaHoaController() {
 		super();
 	}
 
+	List<Hoa> listHoa = new ArrayList<>();
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect(request.getContextPath() + "/baitap/BT2/shop.jsp");
+		HttpSession session = request.getSession();
+		String load = request.getParameter("load");
+		if ("xem".equals(load)) {
+			session.setAttribute("listHoa", listHoa);
+			RequestDispatcher rd = request.getRequestDispatcher("demo/xemhoa.jsp");
+			rd.forward(request, response);
+		} else if ("xoa".equals(load)) {
+			listHoa.clear();
+			session.setAttribute("listHoa", listHoa);
+			RequestDispatcher rd = request.getRequestDispatcher("demo/xemhoa.jsp");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("demo/muahoa.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String strid = request.getParameter("id");
-		String tenHoa = request.getParameter("ten");
-		String strsoLuong = request.getParameter("soluong");
-		String strgia = request.getParameter("gia");
-		if ("".equals(strid) || "".equals(tenHoa) || "".equals(strsoLuong) || "".equals(strgia)) {
-			response.sendRedirect(request.getContextPath() + "/baitap/BT2/shop.jsp?msg=0");
-		} else {
-			int id = 0;
-			int soLuong = 0;
-			int gia = 0;
-			try {
-				id = Integer.parseInt(strid);
-				soLuong = Integer.parseInt(strsoLuong);
-				gia = Integer.parseInt(strgia);
-			} catch (NumberFormatException e) {
-				response.sendRedirect(request.getContextPath() + "/baitap/BT2/shop.jsp?msg=2");
-				return;
-			}
-			boolean check = false;
-			for (Hoa objHoa : listHoa) {
-				if (objHoa.getId() == id) {
-					objHoa.setSoLuong(objHoa.getSoLuong() + soLuong);
-					check = true;
-				}
-			}
-			if (!check) {
-				Hoa hoa = new Hoa(id, tenHoa, soLuong, gia);
-				listHoa.add(hoa);
-			}
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
 
-			HttpSession session = request.getSession();
-			session.setAttribute("listHoa", listHoa);
-
-			response.sendRedirect(request.getContextPath() + "/baitap/BT2/shop.jsp?msg=1");
+		int id = Integer.parseInt(request.getParameter("id"));
+		String tenHoa = request.getParameter("tenhoa");
+		int soLuong = Integer.parseInt(request.getParameter("soluong"));
+		int gia = Integer.parseInt(request.getParameter("gia"));
+		boolean check = false;
+		for (Hoa objHoa : listHoa) {
+			if (objHoa.getId() == id) {
+				objHoa.setSoLuong(soLuong+objHoa.getSoLuong());
+				check=true;
+			}
 		}
+		if(!check) {
+			listHoa.add(new Hoa(id, tenHoa, soLuong, gia));
+		}
+		HttpSession session = request.getSession();
+		session.setAttribute("listHoa", listHoa);
+
+		response.sendRedirect(request.getContextPath() + "/xu-ly-mua-hoa");
 	}
 }
